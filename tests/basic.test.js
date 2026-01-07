71a8027a8e7958d18f6ddb2a895a327bcd763e85
Patricia <delitricia@hotmail.com>
2026-01-07 01:42:32 +0100

    finish tests

--- tests/basic.test.js
+++ tests/basic.test.js
@@ -4,6 +4,11 @@ describe("Variables en JS", () => {
     it("Completa todas las constantes que faltan", () => {
 
         const a = 10;
+        const b = 1.34;
+        const c = true;
+        const d = "Buenos días";
+        const e = "Pepe";
+        const f = "Buenos días, pepe"
 
         expect(a).toBe(10);
         expect(b).toBe(1.34);
@@ -15,11 +20,18 @@ describe("Variables en JS", () => {
     it("Completa todas las variables que faltan para que las operaciones resulten correctamente", () => {
 
         let a = 11;
+        let b = .34;
+        let c = 10;
+        let d = 10;
+        let e = 350;
+        let f = 10;
+        let g = 3;
+        let x = 2;
+        let y = 3;
 
         expect(a + b).toBe(11.34);
         expect(a * c).toBe(110);
         expect(d - e).toBe(-340);
-        // investiga para qué sirven los operadores ** y %
         expect(f ** g).toBe(1000);
         expect(x % 2).toBe(0);
         expect(y % 2).toBe(1);
@@ -27,6 +39,10 @@ describe("Variables en JS", () => {
     it("Completa todas las variables que faltan para que se cumplan las condiciones", () => {
 
         let a = 10;
+        let b = 100;
+        let c = undefined;
+        let d = "Hi!";
+        let n = "Ok";
 
         expect(a > 9).toBeTruthy();
         expect(a < 11).toBeTruthy();
@@ -43,13 +59,14 @@ describe("condicionales en javascript", () => {
         let name = "Pepe";
         let result = "KO";
 
+        expect(name === "Pepe").toBeThruthy;
+        expect(result).toBe("KO");
+
         // https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/String/length
         if (name.length === 6) {
             result = "OK"
-        };
-
-        // substituye ??? por el valor que tiene la variable "result"        
-        expect(result).toBe("???");
+        };       
+        expect(result).toBe("KO");
     });
     it("comparando cadenas (strings)", () => {
 
@@ -64,7 +81,7 @@ describe("condicionales en javascript", () => {
         };
 
         // substituye ??? por el valor que tiene la variable "result"        
-        expect(result).toBe("???");
+        expect(result).toBe("pikachu");
     });
     it("Interpolación de cadenas (strings) (1)", () => {
         let n = "Piña";
@@ -78,9 +95,8 @@ describe("condicionales en javascript", () => {
         } else {
             result = "Pizza con " + m;
         };
-
-        // substituye ??? por el valor que tiene la variable "result"        
-        expect(result).toBe("???");
+       
+        expect(result).toBe("Pizza con Pepperoni");
     });
     it("Interpolación de cadenas (strings) (2)", () => {
         let n = "Piña";
@@ -96,7 +112,7 @@ describe("condicionales en javascript", () => {
         };
 
         // substituye ??? por el valor que tiene la variable "result"        
-        expect(result).toBe("???");
+        expect(result).toBe("Pizza con Piña");
     });
 });
 
@@ -104,24 +120,24 @@ describe("Funciones en JS", () => {
     it('¿Cuál es el resultado de invocar la función getNumber?', () => {
         let result = getNumber();
         // substituye "???" por el valor que tiene la variable "result"
-        expect(result).toBe("???");
+        expect(result).toBe(2);
     });
     it("¿Cuál es el resultado de invocar la función getResult?", () => {
         let result = getResult(10, 30, 2);
         // substituye "???" por el valor que tiene la variable "result"        
-        expect(result).toBe("???");
+        expect(result).toBe(28);
     });
     it("¿Cuál es el resultado de invocar la función getString?", () => {
         let result = getString("Cerebro", "Pinky");
         // substituye "???" por el valor que tiene la variable "result"        
-        expect(result).toBe("???");
+        expect(result).toBe("Son Pinky y Cerebro");
     });
     it("¿Cuál es el resultado de invocar la función getLength?", () => {
         // substituye "???" por el resultado en cada caso
-        expect(getLength("khaleesi mother of dragons breaker of chains")).toBe("???");
-        expect(getLength("sarah")).toBe("???");
-        expect(getLength("bob")).toBe("???");
-        expect(getLength("robertson")).toBe("???");
+        expect(getLength("khaleesi mother of dragons breaker of chains")).toBe("very long");
+        expect(getLength("sarah")).toBe("adecquate");
+        expect(getLength("bob")).toBe("too short");
+        expect(getLength("robertson")).toBe("long");
     });
 });
 
